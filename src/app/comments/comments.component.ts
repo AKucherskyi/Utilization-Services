8d64eb7dd418c17676bf658ad283115cb3dda651
Andrii Kucherskyi <a.kucherski@gmail.com>
2021-12-12 18:42:46 +0200

    stars

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -3,60 +3,70 @@ import { Subject } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { MarkerService } from './../services/marker.service';
 import { Service, Comment } from './../shared/interfaces';
-import { AfterViewChecked, AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { trigger, transition, style, animate } from '@angular/animations';
 import { FormControl, FormGroup } from '@angular/forms';
-
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
   styleUrls: ['./comments.component.scss'],
-  animations: [trigger('enterAnimation', [
-    transition(':enter', [
-      style({ transform: 'translateX(-100%)', opacity: 0 }),
-      animate('350ms', style({ transform: 'translateX(0)', opacity: 1 })),
-    ])
-  ])]
-  
+  animations: [
+    trigger('enterAnimation', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)', opacity: 0 }),
+        animate('350ms', style({ transform: 'translateX(0)', opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
-export class CommentsComponent implements OnInit {
-  service!: Service | null
-  form!: FormGroup
+export class CommentsComponent implements OnInit, AfterViewChecked {
+  service!: Service | null;
+  form!: FormGroup;
 
-  @Output() close: EventEmitter<any> = new EventEmitter()
+  @Output() close: EventEmitter<any> = new EventEmitter();
 
   @ViewChild('list') list!: ElementRef;
 
-  constructor(private markerService: MarkerService) { }
- 
+  constructor(private markerService: MarkerService) {}
+
+  ngAfterViewChecked(): void {
+    if (this.list) {
+      this.list.nativeElement.scrollTop = 9999;
+    }
+  }
 
   ngOnInit(): void {
-
     this.form = new FormGroup({
-      content: new FormControl('')
-    })
+      content: new FormControl(''),
+    });
 
-    this.markerService.currentServiceId$.pipe(
-      switchMap((id) => this.markerService.getService(id))
-    )
-    .subscribe((service) => {
-      this.service = service
-    })    
+    this.markerService.currentServiceId$
+      .pipe(switchMap((id) => this.markerService.getService(id)))
+      .subscribe((service) => {
+        this.service = service;
+      });
   }
 
   sendComment() {
     if (this.service) {
-      this.markerService.postComment(this.service.service_id, this.form.value.content).subscribe((comment: Comment) => {
-        this.form.reset();
-      this.service?.comments?.push(comment)  
-      this.list.nativeElement.scrollTop = 9999
-      console.log(this.list.nativeElement);
-      
-      
-    })
+      this.markerService
+        .postComment(this.service.service_id, this.form.value.content)
+        .subscribe((comment: Comment) => {
+          this.form.reset();
+          this.service?.comments?.push(comment);
+        });
     }
-    
   }
-
 }
